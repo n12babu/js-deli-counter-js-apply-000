f766d4449f7729d5f897e22072627762517f0889
Nazrul Babu <n12babu@siena.edu>
2018-12-10 20:43:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const takeANumber = (line, name)=>{
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 
-}
+};
 
 const nowServing = (line)=>{
   if (line.length===0)
@@ -10,17 +10,16 @@ const nowServing = (line)=>{
   else
     return `Currently serving ${line.shift()}.`
 
-}
+};
+
 
 
 const currentLine = (line)=>{
-  if (line.length===0){
-    return `The line is currently empty.`;
-  }
+if (line.length===0){
+  return `The line is currently empty.`
+}
 let position = [];
 for (let i= 0; i<line.length; i++){
-  position.push (i+1+'. '+line[i])
-}
-return `The line is currently: ${position.join(', ')}`
-
+  position.push(i+1+ '. '+ line[i])
+}return `The line is currently: ${position.join(', ')}`
 };
